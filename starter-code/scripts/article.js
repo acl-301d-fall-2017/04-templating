505d9b3940664a5d776284294daa415cf12f4bb7
Richard Livingston <livingston.richard@gmail.com>
2017-11-28 15:28:34 -0800

    getting rid of linter errors now that my linter is working again.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -1,14 +1,14 @@
 'use strict';
 
-let articles = [];
+let articles = []; // eslint-disable-line
 
 function Article (rawDataObj) {
-  this.author = rawDataObj.author;
-  this.authorUrl = rawDataObj.authorUrl;
-  this.title = rawDataObj.title;
-  this.category = rawDataObj.category;
-  this.body = rawDataObj.body;
-  this.publishedOn = rawDataObj.publishedOn;
+    this.author = rawDataObj.author;
+    this.authorUrl = rawDataObj.authorUrl;
+    this.title = rawDataObj.title;
+    this.category = rawDataObj.category;
+    this.body = rawDataObj.body;
+    this.publishedOn = rawDataObj.publishedOn;
 }
 
 Article.prototype.toHtml = function() {
@@ -18,8 +18,8 @@ Article.prototype.toHtml = function() {
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000); // eslint-disable-line
+    this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
 
   // []TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
 
@@ -28,13 +28,13 @@ Article.prototype.toHtml = function() {
 // []COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
 // PUT YOUR RESPONSE HERE
 rawData.sort((a,b) => {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
 
 rawData.forEach(articleObject => {
-  articles.push(new Article(articleObject));
+    articles.push(new Article(articleObject));
 });
 
 articles.forEach(article => {
-  $('#articles').append(article.toHtml());
+    $('#articles').append(article.toHtml());
 });
