67d08da9cf3af9bb7a3313587e49cd13c7bbe5af
Richard Livingston <livingston.richard@gmail.com>
2017-11-28 16:29:18 -0800

    got the template to work, and the toHtml to work as well. Getting ready to re factor functions to arrow functions.

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -12,21 +12,25 @@ function Article (rawDataObj) {
 }
 
 Article.prototype.toHtml = function() {
-  // []TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+  // [X]TODO: Use Handlebars to render your articles. Get your template from the DOM and "compile" your template with Handlebars.
+    const theTemplateScript = $('#article-template').html();
+    const theTemplate = Handlebars.compile(theTemplateScript);
 
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn)) / 60 / 60 / 24 / 1000);
+    this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
+
+    const filledTemplate = theTemplate(this);
+    return filledTemplate;
   // REVIEW: If your template will use properties that aren't on the object yet, add them.
   // Since your template can't hold any JS logic, we need to execute the logic here.
   // The result is added to the object as a new property, which can then be referenced by key in the template.
   // For example, you might want to display how old a post is, or say "(draft)" if it has no publication date:
-    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000); // eslint-disable-line
-    this.publishStatus = this.publishedOn ? `published ${this.daysAgo} days ago` : '(draft)';
-
-  // []TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
+  // [X]TODO: Use the method that Handlebars gave you to return your filled-in html template for THIS article.
 
 };
 
 // []COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// It is not around them because they are on the same line. And you use different syntax for very short arrow functions. For some reason.
 rawData.sort((a,b) => {
     return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
@@ -38,3 +42,4 @@ rawData.forEach(articleObject => {
 articles.forEach(article => {
     $('#articles').append(article.toHtml());
 });
+Article.prototype.toHtml();
