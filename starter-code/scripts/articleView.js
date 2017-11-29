2afb8ace7d86697cc539d321cc62fcf009cad57a
CharlyWelch <blwbiology@gmail.com>
2017-11-28 17:19:37 -0800

    populate filters with handlebars - repeats in categories

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -6,41 +6,36 @@ const articleView = {};
 
 // COMMENT: How do arrow functions affect the context of "this"? How did you determine if a function could be refactored?
 // Arrow functions take 'this' out of the scope of the function, so 'this' will refer to the window if used in an arrow function. A function can be refactored if it is not dependent on object-specific properties. You can also use arrow functions on the outside function if other fns nested inside it use 'this'.  
-const authorTemplate = $('#author-template').html();
-const templateFiller = Handlebars.compile(authorTemplate);
-
-// articleView.populateFilters = () => {
-//     $('article').each(function() {
-
-//         const filledTemplate = templateFiller(this);
-//         return filledTemplate;
-
-//     });
-// };
 const authors = [];
-rawData.forEach(function(element) {
-    authors.push(element.author);
-});
 
-authors.forEach(author => {
-    const filledTemplate = templateFiller(author);
-    $('#author-filter').append(filledTemplate);
-});
+articleView.populateAuthors = () => {
+    rawData.forEach(function(element) {
+        authors.push(element.author);
+    });
+        
+    const authorTemplate = $('#author-template').html();
+    const templateFiller = Handlebars.compile(authorTemplate);
+    authors.forEach(author => {
+        const filledTemplate = templateFiller({author});
+        $('#author-filter').append(filledTemplate);
+    });
+};
 
-// if (!$(this).hasClass('template')) {
-//     let val = $(this).find('address a').text();
-//     let optionTag = `<option value="${val}">${val}</option>`;
+const categories = [];
 
-//     if ($(`#author-filter option[value="${val}"]`).length === 0) {
-//         $('#author-filter').append(optionTag);
-//     }
+articleView.populateCategories = () => {
+    rawData.forEach(function(element) {
+        categories.push(element.category);
+    });
+        
+    const categoryTemplate = $('#category-template').html();
+    const templateFiller = Handlebars.compile(categoryTemplate);
+    categories.forEach(category => {
+        const filledTemplate = templateFiller({category});
+        $('#category-filter').append(filledTemplate);
+    });
+};
 
-//     val = $(this).attr('data-category');
-//     optionTag = `<option value="${val}">${val}</option>`;
-//     if ($(`#category-filter option[value="${val}"]`).length === 0) {
-//         $('#category-filter').append(optionTag);
-//     }
-// }
 articleView.handleAuthorFilter = () => {
     $('#author-filter').on('change', function() {
         if ($(this).val()) {
@@ -96,7 +91,8 @@ articleView.setTeasers = () => {
 };
 
 $(document).ready(() => {
-    articleView.populateFilters();
+    articleView.populateAuthors();
+    articleView.populateCategories();
     articleView.handleCategoryFilter();
     articleView.handleAuthorFilter();
     articleView.handleMainNav();
